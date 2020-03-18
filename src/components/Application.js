74be7a319a2b1bcfa0b4c7356c77d24dada1982d
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-17 20:54:08 -0700

    can view appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,34 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment/index";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "5pm"
+  }
+];
 
 const days = [
   {
@@ -43,7 +71,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} interview={appointment.interview} />
+        ))}
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,21 @@
 // import Appointment from "components/Appointment";
 import React from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  console.log(props);
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {!props.interview && <Empty />}
+    </article>
+  );
 }
