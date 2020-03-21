db3bfbb7544a1d559acdb7cfc2942bf8a6284d85
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-21 12:42:38 -0700

    cleaned up code and formating

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
+import "components/Application.scss";
 import axios from "axios";
 
+//----------------imported to Application.js-------------------------
 export default function useApplicationData() {
   const setDay = day => setState(prev => ({ ...prev, day }));
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
+  //-------------------GET request with promises-------------------------
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
@@ -25,6 +29,7 @@ export default function useApplicationData() {
     });
   }, []);
 
+  //--------------cancelInterview function uses promises------------------
   function cancelInterview(id, interview) {
     console.log(id, interview);
     const appointment = {
@@ -35,12 +40,11 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    axios
+    return axios
       .delete(`/api/appointments/${id}`, appointment)
-      .then(setState({ ...state, appointments }))
-      .catch(err => console.error(err));
+      .then(setState({ ...state, appointments }));
   }
-
+  //--------------bookInterview function uses promises------------------
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -53,6 +57,13 @@ export default function useApplicationData() {
     return axios
       .put(`http://localhost:8001/api/appointments/${id}`, appointment)
       .then(setState({ ...state, appointments }));
-    // .catch(err => console.error(err));
   }
+  //------------------Change remaining days-----------------------------
+  useEffect(() => {
+    axios
+      .get("/api/days")
+      .then(days => setState(state => ({ ...state, days: days.data })));
+  }, [state.appointments]);
+
+  return { state, setDay, bookInterview, cancelInterview };
 }
